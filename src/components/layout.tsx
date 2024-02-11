b9973ef385d97d31cb3a0e48ae34882ed1c3a693
Yannick <tech@yalit.be>
2024-02-11 20:49:49 +0100

    feat : add pictures in index

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,30 @@
 import { Link } from "gatsby";
-import React from "react";
+import React, { ReactNode } from "react";
+import { global_content_offset_y } from "../styles/layout.module.scss";
 import { header, container, main, footer } from "../styles/layout.module.scss";
 import { StaticImage } from "gatsby-plugin-image";
+
+type NavLinkType = { display: ReactNode; link: string };
+const navLinks: NavLinkType[] = [
+  {
+    display: (
+      <StaticImage
+        src="../images/layout/logo.png"
+        alt="Logo Yalit"
+        placeholder="blurred"
+        layout="fixed"
+        width={30}
+        height={30}
+      />
+    ),
+    link: "/",
+  },
+  { display: "About", link: "/about" },
+  { display: "Blog", link: "/blog" },
+  { display: "Projects", link: "/projects" },
+  { display: "Photos", link: "/photos" },
+  { display: "Contact", link: "/contact" },
+];
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -10,39 +33,28 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <nav>
           <div className="flex-1"></div>
           <ul>
-            <li>
-              <Link to="/">
-                <StaticImage
-                  src="../images/layout/logo.png"
-                  alt="Logo Yalit"
-                  placeholder="blurred"
-                  layout="fixed"
-                  width={30}
-                  height={30}
-                />
-              </Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/blog">Blog</Link>
-            </li>
-            <li>
-              <Link to="/projects">Projects</Link>
-            </li>
-            <li>
-              <Link to="/photos">Photos</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
+            {navLinks.map((navLink, index) => (
+              <li key={index}>
+                <Link to={navLink.link}>{navLink.display}</Link>
+              </li>
+            ))}
           </ul>
           <div className="flex-1"></div>
         </nav>
       </header>
+
+      <div className={global_content_offset_y}></div>
+
       <main className={main}>{children}</main>
+
       <footer className={footer}>
+        <nav>
+          {navLinks.map((navLink, index) => (
+            <Link key={index} to={navLink.link}>
+              {navLink.display}
+            </Link>
+          ))}
+        </nav>
         <div className="flex-1"></div>
         <div className="trademark">
           © {new Date().getFullYear()} Yalit Consultancy. All Rights Reserved,
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,59 @@
 import { Link, PageProps } from "gatsby";
 import * as React from "react";
 import Layout from "../components/layout";
-import { global_content_offset_y } from "../styles/layout.module.scss";
-import { summary } from "../styles/index.module.scss";
+import { summary, banner, bannerImage } from "../styles/index.module.scss";
 import LinkedinIcon from "../components/icons/linkedin";
 import GithubIcon from "../components/icons/github";
 import Icon500px from "../components/icons/500px";
 import InstagramIcon from "../components/icons/instagram";
 import { StaticImage } from "gatsby-plugin-image";
+import classnames from "../helpers/classnames";
+
+type BannerImageType = { src: string; alt: string };
+
+const bannerImages: BannerImageType[] = [
+  { src: "../images/index/coast.jpg", alt: "Coast with cliffs and nature" },
+  { src: "../images/index/cow.jpg", alt: "Coast with cliffs and nature" },
+  { src: "../images/index/source.jpg", alt: "Coast with cliffs and nature" },
+  { src: "../images/index/wood_nb.jpg", alt: "Coast with cliffs and nature" },
+  {
+    src: "../images/index/tree_light.jpg",
+    alt: "Coast with cliffs and nature",
+  },
+];
 
 const IndexPage: React.FC<PageProps> = ({ data }) => {
   return (
     <Layout>
-      <div className={global_content_offset_y}></div>
       <div className={summary}>
         <div className="title">
-          <p>Application developer, analyst, and project manager.</p>
-          <p className="ml-40">Daydreams photographer</p>
+          <p>Developer</p>
+          <p className="ml-10">Analyst</p>
+          <p className="ml-20">Project manager</p>
+          <p className="ml-32">Daydreams photographer</p>
+          <StaticImage
+            src="../images/index/me.jpg"
+            alt="Me"
+            width={100}
+            height={100}
+            className="me"
+          />
         </div>
         <div className="description">
           <div>
             <p>
-              I'm Yannick, an application engineer, analyst, and project
-              manager. I have a passion for technology and I love to engineer
-              and build things. I have a strong background in software
-              development, project management and business analysis. I have
-              worked in various industries including finance, insurance, and
-              healthcare. I thrive in a fast-paced environment and I am always
-              looking for new challenges.
+              I'm Yannick, a developer, analyst, and project manager. I have a
+              passion for technology and I love to find solutions and build
+              things. I have a strong background in software development,
+              project management and business analysis. I have worked in various
+              industries including finance, insurance, and healthcare. I thrive
+              in a fast-paced environment and I am always looking for new
+              challenges.
+              <br />
+              My favorite stack is <Link to="https://react.dev/">React</Link>
+              &nbsp;and <Link to="https://www.symfony.com">Symfony</Link>, but I
+              have experience with other technologies as well. I am always
+              looking to learn new things and I am open to new opportunities.
             </p>
             <StaticImage
               className="rounded_img rotate-2"
@@ -39,12 +65,12 @@ const IndexPage: React.FC<PageProps> = ({ data }) => {
           </div>
           <div>
             <p>
-              I'm also a passionate photographer. I love to capture the beauty
-              of the world and share it with others. I have a strong interest in
-              nature, landscapes, and urban photography. I love to explore new
-              places and capture the beauty of the world. I'm always looking for
-              new opportunities to collaborate with other photographers and
-              artists.
+              I'm also a passionate photographer. <br />I love to capture the
+              beauty of the world and share it with others. I have a strong
+              interest in nature, landscapes, and urban photography. <br />I
+              love to explore new places and capture the beauty of the world.
+              I'm always looking for new opportunities to collaborate with other
+              photographers and artists.
             </p>
             <StaticImage
               className="rounded_img -rotate-2"
@@ -74,7 +100,33 @@ const IndexPage: React.FC<PageProps> = ({ data }) => {
         </div>
       </div>
 
-      <div className="banner"></div>
+      <div className={banner}>
+        <StaticImage
+          className={classnames(bannerImage, "-rotate-3")}
+          src="../images/index/coast.jpg"
+          alt="Coast with cliffs and nature"
+        />
+        <StaticImage
+          className={classnames(bannerImage, "rotate-2")}
+          src="../images/index/cow.jpg"
+          alt="Coast with cliffs and nature"
+        />
+        <StaticImage
+          className={classnames(bannerImage, "-rotate-1")}
+          src="../images/index/source.jpg"
+          alt="Coast with cliffs and nature"
+        />
+        <StaticImage
+          className={classnames(bannerImage, "rotate-2")}
+          src="../images/index/wood_nb.jpg"
+          alt="Coast with cliffs and nature"
+        />
+        <StaticImage
+          className={classnames(bannerImage, "-rotate-3")}
+          src="../images/index/tree_light.jpg"
+          alt="Coast with cliffs and nature"
+        />
+      </div>
     </Layout>
   );
 };
