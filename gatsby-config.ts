6283cc69fb99e980aef93aff177da468ae13c6ad
Yannick <tech@yalit.be>
2024-04-05 23:59:42 +0200

    build : update path-prefix

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,6 +1,7 @@
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
+  pathPrefix: `/yalit-site`,
   siteMetadata: {
     title: `Yalit`,
     siteUrl: `https://www.yourdomain.tld`,
