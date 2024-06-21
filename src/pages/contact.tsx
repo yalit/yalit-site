f94d1f1145c0f9083c2c13e2bb0dce6b522e4cee
Yannick <tech@yalit.be>
2024-06-21 22:02:24 +0200

    feat : update contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -8,7 +8,7 @@ export default function Contact() {
             <div className="flex flex-col gap-10 items-center justify-center h-[50vh]">
                 <div className="flex justify-center items-center gap-3">
                     <FontAwesome icon="arobase" className="h-5 w-7 fill-gray-800 mt-3" />
-                    <h1 className="text-4xl font-semibold text-gray-800">contact[@]yalit.be</h1>
+                    <h1 className="text-4xl font-semibold text-gray-800">contact[at]yalit.be</h1>
                 </div>
             </div>
         </Layout>
