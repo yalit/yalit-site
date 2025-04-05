a9a7d6fabc55d0cb07caa7d40593177412916fbe
Yannick <tech@yalit.be>
2025-04-05 18:13:50 +0200

    fix : fix contact page and footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,10 +22,10 @@ export default function Layout({Component, pageProps}: AppProps) {
                 </main>
 
                 <footer
-                    className="flex flex-col md:flex-row justify-start md:justify-between items-center flex-wrap border-t border-zinc-100  md:px-4 lg:px-8">
+                    className="flex flex-col md:flex-row justify-start md:justify-between items-center flex-wrap border-t border-zinc-100 px-5 lg:px-8 pb-2">
                     <Navigation type="bottom"/>
                     <div className="flex-1"></div>
-                    <div className="text-zinc-800 text-xs italic">
+                    <div className="text-zinc-800 text-xs italic text-center">
                         © {new Date().getFullYear()} Yalit Consultancy. All Rights Reserved,
                         Statically built with Next.js
                     </div>
