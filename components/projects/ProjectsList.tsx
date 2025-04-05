1a200bf9f686a7f5821f565fb9c4983858cb5078
Yannick <tech@yalit.be>
2025-04-05 18:01:22 +0200

    fix : fix project list

--- components/projects/ProjectsList.tsx
+++ components/projects/ProjectsList.tsx
@@ -3,7 +3,7 @@ import ProjectsListProject from "./ProjectsListProject";
 
 export default function ProjectList({ projects }: { projects: ProjectInformation[] }) {
     return (
-        <div className="project-list flex flex-col gap-[3rem]">
+        <div className="project-list flex flex-col gap-[3rem] px-5 md:px-8">
             {projects.map((project: ProjectInformation) => <ProjectsListProject project={project} key={project.title} />)}
         </div>
     )
