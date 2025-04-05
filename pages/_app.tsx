a0bb2378220b753ff81ad39631a529abf2f39f8c
Yannick <tech@yalit.be>
2025-04-05 12:43:05 +0200

    fix : fix index page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,30 +1,36 @@
-import { AppProps } from "next/app";
+import {AppProps} from "next/app";
 import '../styles/globals.css'
 import classnames from "@/lib/classnames";
-import { Navigation } from "@/components/shared/Navigation";
+import {Navigation} from "@/components/shared/Navigation";
+import Head from "next/head";
 
-export default function Layout({ Component, pageProps }: AppProps) {
+export default function Layout({Component, pageProps}: AppProps) {
     const containerClassNames = classnames("container-perso");
 
     return (
-        <div className={containerClassNames}>
-            <header className="header">
-                <Navigation type="top" />
-            </header>
+        <>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            </Head>
+            <div className={containerClassNames}>
+                <header className="header">
+                    <Navigation type="top"/>
+                </header>
 
-            <main className="pt-[var(--content-offset)] flex-1">
-                <Component {...pageProps} />
-            </main>
+                <main className="pt-[20px] sm:pt-[80px] flex-1">
+                    <Component {...pageProps} />
+                </main>
 
-            <footer className="flex flex-col md:flex-row justify-start md:justify-between items-center flex-wrap border-t border-zinc-100  md:px-4 lg:px-8">
-                <Navigation type="bottom" />
-                <div className="flex-1"></div>
-                <div className="text-zinc-800 text-xs italic">
-                    © {new Date().getFullYear()} Yalit Consultancy. All Rights Reserved,
-                    Statically built with Next.js
-                </div>
-            </footer>
-        </div>
-
+                <footer
+                    className="flex flex-col md:flex-row justify-start md:justify-between items-center flex-wrap border-t border-zinc-100  md:px-4 lg:px-8">
+                    <Navigation type="bottom"/>
+                    <div className="flex-1"></div>
+                    <div className="text-zinc-800 text-xs italic">
+                        © {new Date().getFullYear()} Yalit Consultancy. All Rights Reserved,
+                        Statically built with Next.js
+                    </div>
+                </footer>
+            </div>
+        </>
     )
 }
